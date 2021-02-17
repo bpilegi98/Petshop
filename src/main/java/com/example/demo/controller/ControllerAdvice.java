6e468a40d3d2bb77274759de4c662c938aeffc8e
Bianca Pilegi <bpilegi@mobydigital.com>
2021-02-17 12:15:02 -0300

    Agregando Log a ErrorDto y eliminándolo de controller
    (Chequear que esté bien implementado el cambio)

--- src/main/java/com/example/demo/controller/ControllerAdvice.java
+++ src/main/java/com/example/demo/controller/ControllerAdvice.java
@@ -3,24 +3,35 @@ package com.example.demo.controller;
 import com.example.demo.exceptions.PetshopAlreadyExistsException;
 import com.example.demo.exceptions.PetshopNotExistsException;
 import dto.ErrorDto;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @org.springframework.web.bind.annotation.ControllerAdvice
+
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
+
+    private final static Logger log = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
     @ExceptionHandler(PetshopAlreadyExistsException.class)
     public ResponseEntity<ErrorDto> handleAlreadyExistsException(PetshopAlreadyExistsException exception)
     {
+
         ErrorDto error = new ErrorDto(
                 HttpStatus.BAD_REQUEST.value(),
                 new Date(),
-                exception.getMessage()
+                exception.getMessage(),
+                log
         );
+
+        //Chequear si esta bien
+        error.getLog().log(Level.WARNING, "Couldn't add");
 
         return new ResponseEntity<ErrorDto>(error, HttpStatus.BAD_REQUEST);
     }
@@ -31,7 +42,8 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
         ErrorDto error = new ErrorDto(
                 HttpStatus.NOT_FOUND.value(),
                 new Date(),
-                exception.getMessage()
+                exception.getMessage(),
+                log
         );
 
         return new ResponseEntity<ErrorDto>(error, HttpStatus.NOT_FOUND);
@@ -43,7 +55,8 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
         ErrorDto error = new ErrorDto(
                 HttpStatus.BAD_REQUEST.value(),
                 new Date(),
-                exception.getMessage()
+                exception.getMessage(),
+                log
         );
 
         return new ResponseEntity<ErrorDto>(error, HttpStatus.BAD_REQUEST);
--- src/main/java/dto/ErrorDto.java
+++ src/main/java/dto/ErrorDto.java
@@ -2,8 +2,10 @@ package dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.extern.java.Log;
 
 import java.util.Date;
+import java.util.logging.Logger;
 
 @AllArgsConstructor
 @Data
@@ -12,4 +14,7 @@ public class ErrorDto {
     private int statusCode;
     private Date timestamp;
     private String message;
+    private Logger log;
+
+
 }
